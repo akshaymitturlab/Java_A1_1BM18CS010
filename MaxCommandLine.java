5cea5aa6b6a8d183c400fda28b5952e1a6112007
Akshay Mittur <53973656+akshaymitturlab@users.noreply.github.com>
2019-09-06 14:28:10 +0530

    Create MaxCommandLine.java

---
+++ MaxCommandLine.java
@@ -0,0 +1,9 @@
+public class MaxCommandLine {
+	public static void main(String[] args) {
+		int max = 0;
+		for (int i = 0; i < args.length; ++i) {
+		    if (max < Integer.parseInt(args[i])) max = Integer.parseInt(args[i]);
+		}
+		System.out.println(max);
+	}
+}
